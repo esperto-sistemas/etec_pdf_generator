604cd680645acb82fd097ed42cb558573d2831cc
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-04-01 22:25:57 -0300

    feat: add dynamic equipments fields

--- src/routes/generate-pdf/pdfs/sections/equipments.ts
+++ src/routes/generate-pdf/pdfs/sections/equipments.ts
@@ -1,54 +1,106 @@
-import { headerTextColor, marginHorizontal, paddingBetweenColumns } from '../constants'
-import { drawHeader, drawSubtitle, drawText, spaceBetweenSections } from '../helpers'
+import { GeneratePDFBody } from 'types/PdfBody'
+import { marginHorizontal } from '../constants'
+import { drawHeader, drawText, spaceBetweenSections } from '../helpers'
 
-export function renderEquipment(doc: PDFKit.PDFDocument, currentY: number, visit = false) {
+type RenderEquipmentData = Pick<
+  GeneratePDFBody,
+  | 'modelo'
+  | 'garantia'
+  | 'estagio'
+  | 'tipoIntervencao'
+  | 'tipoQueimador'
+  | 'aplicacao'
+  | 'equipamentoQuantidade'
+>
+
+export function renderEquipment(
+  doc: PDFKit.PDFDocument,
+  currentY: number,
+  visit = false,
+  data: RenderEquipmentData,
+) {
   drawHeader(doc, 'EQUIPAMENTO')
 
+  const textGarantia = data.garantia === 'COM_GARANTIA' ? 'Com garantia' : 'Sem garantia'
+
+  currentY = doc.y
+  drawText(doc, `Nome: ${data.modelo.equipamento.nome}`)
+  doc.y = currentY
+  drawText(doc, `Marca: ${data.modelo.marca.nome}`, { align: 'center' })
+  doc.y = currentY
+  drawText(doc, `Modelo: ${data.modelo.nome}`, { align: 'right' })
+
+  doc.moveDown(0.5)
+
   if (visit) {
-    currentY = doc.y
-    const columnWidth = (doc.page.width - marginHorizontal * 2 - paddingBetweenColumns) / 2
-    drawSubtitle(doc, 'Descrição', { width: columnWidth, align: 'left' }, headerTextColor)
+    const estagio = (() => {
+      switch (data.estagio) {
+        case 'I_ESTAGIO':
+          return '1º Estágio'
+        case 'II_ESTAGIO':
+          return '2º Estágio'
+        case 'MODULANTE':
+          return 'Modulante'
+        default:
+          return 'Não informado'
+      }
+    })()
 
-    doc.x = marginHorizontal + columnWidth + paddingBetweenColumns
-    doc.y = currentY
-    drawSubtitle(doc, 'Quantidade', { width: columnWidth, align: 'left' }, headerTextColor)
+    const textTipoQueimador = (() => {
+      switch (data.tipoQueimador) {
+        case 'GAS_GLP':
+          return 'GLP'
+        case 'GAS_NATURAL':
+          return 'Gás natural'
+        case 'DIESEL':
+          return 'Diesel'
+        default:
+          return 'Não informado'
+      }
+    })()
 
-    doc.moveDown(0.5)
-
-    const equipmentDetails = [
-      { description: 'Marca: Queimador', quantity: 3 },
-      { description: 'Modelo: Baltur', quantity: 3 },
-      { description: 'Estágio: x', quantity: 3 },
-    ]
-
-    equipmentDetails.forEach((detail) => {
-      currentY = doc.y
-      doc.x = marginHorizontal
-      drawText(doc, detail.description, { width: columnWidth, align: 'left' })
-
-      doc.y = currentY
-      doc.x = marginHorizontal + columnWidth + paddingBetweenColumns
-      drawText(doc, detail.quantity.toString(), { width: columnWidth, align: 'left' })
-
-      doc.moveDown(0.5)
-    })
-
-    doc.moveDown(0.5)
+    const textAplicacao = (() => {
+      switch (data.aplicacao) {
+        case 'ESTUFA_DE_CURA_PO':
+          return 'Estufa de cura pó'
+        case 'ESTUFA_DE_CURA_LIQUIDA':
+          return 'Estufa de cura líquida'
+        case 'BANHO':
+          return 'Banho'
+        case 'SECAGEM':
+          return 'Secagem'
+        case 'OUTROS':
+          return 'Outros'
+        default:
+          return 'Não informado'
+      }
+    })()
 
     doc.x = marginHorizontal
-    drawText(doc, 'Tipo de queimador: Gás natural', { continued: true })
-    drawText(doc, 'Aplicação: Estufa de cura pó', { align: 'right' })
+    drawText(doc, `Quantidade: ${data.equipamentoQuantidade || 1}`, { continued: true })
+    drawText(doc, `Estágio: ${estagio}`, { align: 'right' })
+    doc.moveDown(0.5)
+    drawText(doc, `Tipo de queimador: ${textTipoQueimador}`, { continued: true })
+    drawText(doc, `Aplicação: ${textAplicacao}`, { align: 'right' })
   } else {
-    currentY = doc.y
-    drawText(doc, 'Nome: Queimadores')
-    doc.y = currentY
-    drawText(doc, 'Marca: Baltur', { align: 'center' })
-    doc.y = currentY
-    drawText(doc, 'Modelo: B 1000', { align: 'right' })
+    const textIntervencao = (() => {
+      switch (data.tipoIntervencao) {
+        case 'CHAMADA_EXTRA':
+          return 'Chamada extra'
+        case 'CONTRATO':
+          return 'Contrato'
+        case 'OFICINA':
+          return 'Oficina'
+        case 'PROGRAMADA':
+          return 'Programada'
+        default:
+          return 'Não informado'
+      }
+    })()
 
     doc.moveDown(0.5)
-    drawText(doc, 'Sem garantia', { continued: true })
-    drawText(doc, 'Tipo de intervenção: Programada', { align: 'right' })
+    drawText(doc, textGarantia, { continued: true })
+    drawText(doc, `Tipo de intervenção: ${textIntervencao}`, { align: 'right' })
   }
 
   spaceBetweenSections(doc)
